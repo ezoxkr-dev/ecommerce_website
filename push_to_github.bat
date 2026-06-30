14659b70c04711007fccdd4c21961871316492f2
Ezox Admin <admin@ezoxkr.dev>
2026-06-30 23:03:50 +0600

    Initial commit

--- push_to_github.bat
+++ push_to_github.bat
@@ -7,7 +7,7 @@ echo Configuring Git and pushing to GitHub...
 "C:\Program Files\Git\cmd\git.exe" commit -m "Initial commit"
 "C:\Program Files\Git\cmd\git.exe" branch -M main
 "C:\Program Files\Git\cmd\git.exe" remote add origin https://github.com/ezoxkr-dev/ecommerce_website.git 2>nul
-"C:\Program Files\Git\cmd\git.exe" push -u origin main
+"C:\Program Files\Git\cmd\git.exe" push -u origin main --force
 echo.
 echo Done!
 pause
