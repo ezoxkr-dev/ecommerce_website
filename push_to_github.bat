ccf2c3a4eae9c0e6d3fa5a83cddc3dffc3892711
Ezox Admin <admin@ezoxkr.dev>
2026-06-30 23:37:36 +0600

    Initial commit

---
+++ push_to_github.bat
@@ -0,0 +1,13 @@
+@echo off
+echo Configuring Git and pushing to GitHub...
+"C:\Program Files\Git\cmd\git.exe" config user.email "admin@ezoxkr.dev"
+"C:\Program Files\Git\cmd\git.exe" config user.name "Ezox Admin"
+"C:\Program Files\Git\cmd\git.exe" init
+"C:\Program Files\Git\cmd\git.exe" add .
+"C:\Program Files\Git\cmd\git.exe" commit -m "Initial commit"
+"C:\Program Files\Git\cmd\git.exe" branch -M main
+"C:\Program Files\Git\cmd\git.exe" remote add origin https://github.com/ezoxkr-dev/ecommerce_website.git 2>nul
+"C:\Program Files\Git\cmd\git.exe" push -u origin main --force
+echo.
+echo Done!
+pause
